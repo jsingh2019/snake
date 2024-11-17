c5c591ad353a7e8ef71faeecac61cf5b9286627a
jsingh5@stanford.edu <jsingh5@stanford.edu>
2024-11-17 10:39:19 -0800

    first pass at random - to be changed to functions

--- baselineRandomActionPolicy.py
+++ baselineRandomActionPolicy.py
@@ -3,3 +3,11 @@
 
 #random actions. 
 
+def randomPolicy(currDir):
+    import random
+
+    dirs = {'UP': 0.25, 'DOWN': 0.25, 'LEFT': 0.25, 'RIGHT': 0.25}
+    opps = {'UP': 'DOWN', 'DOWN': 'UP', 'LEFT': 'RIGHT', "RIGHT": "LEFT"}
+    currOpp = opps[currDir]
+    validDirections = [d for d in dirs.keys() if d != currOpp]
+    return random.choose(validDirections)
